26209f7f2539399b2372dab88eabf466877b37af
Jch <jinchenghao1120@163.co>
2019-06-18 23:41:07 +0800

    添加连个pojo

---
+++ src/main/java/edu/nuaa/watermeter/pojo/User.java
@@ -0,0 +1,69 @@
+package edu.nuaa.watermeter.pojo;
+
+public class User {
+	private String comunityCode;
+	private String unitCode;
+	private String roomCode;
+	private String userCode;
+	private String userName;
+	private String userPhone;
+	private String simccid;
+	private String cityCode;
+	private String meterAddress;
+	public String getComunityCode() {
+		return comunityCode;
+	}
+	public void setComunityCode(String comunityCode) {
+		this.comunityCode = comunityCode;
+	}
+	public String getUnitCode() {
+		return unitCode;
+	}
+	public void setUnitCode(String unitCode) {
+		this.unitCode = unitCode;
+	}
+	public String getRoomCode() {
+		return roomCode;
+	}
+	public void setRoomCode(String roomCode) {
+		this.roomCode = roomCode;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getSimccid() {
+		return simccid;
+	}
+	public void setSimccid(String simccid) {
+		this.simccid = simccid;
+	}
+	public String getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+	public String getMeterAddress() {
+		return meterAddress;
+	}
+	public void setMeterAddress(String meterAddress) {
+		this.meterAddress = meterAddress;
+	}
+	
+	
+}
---
+++ src/main/java/edu/nuaa/watermeter/pojo/WmStatus.java
@@ -0,0 +1,83 @@
+package edu.nuaa.watermeter.pojo;
+
+public class WmStatus {
+	private String readId;
+	private String meterAddress;
+	private String meterReading;
+	private String recordTime;
+	private String batteryVoltage;
+	private String signalIndicator;
+	private String meterImageWidth;
+	private String meterImageHeight;
+	private String meterImage;
+	private String cityCode;
+	private String grayImage;
+	public String getReadId() {
+		return readId;
+	}
+	public void setReadId(String readId) {
+		this.readId = readId;
+	}
+	public String getMeterAddress() {
+		return meterAddress;
+	}
+	public void setMeterAddress(String meterAddress) {
+		this.meterAddress = meterAddress;
+	}
+	public String getMeterReading() {
+		return meterReading;
+	}
+	public void setMeterReading(String meterReading) {
+		this.meterReading = meterReading;
+	}
+	public String getRecordTime() {
+		return recordTime;
+	}
+	public void setRecordTime(String recordTime) {
+		this.recordTime = recordTime;
+	}
+	public String getBatteryVoltage() {
+		return batteryVoltage;
+	}
+	public void setBatteryVoltage(String batteryVoltage) {
+		this.batteryVoltage = batteryVoltage;
+	}
+	public String getSignalIndicator() {
+		return signalIndicator;
+	}
+	public void setSignalIndicator(String signalIndicator) {
+		this.signalIndicator = signalIndicator;
+	}
+	public String getMeterImageWidth() {
+		return meterImageWidth;
+	}
+	public void setMeterImageWidth(String meterImageWidth) {
+		this.meterImageWidth = meterImageWidth;
+	}
+	public String getMeterImageHeight() {
+		return meterImageHeight;
+	}
+	public void setMeterImageHeight(String meterImageHeight) {
+		this.meterImageHeight = meterImageHeight;
+	}
+	public String getMeterImage() {
+		return meterImage;
+	}
+	public void setMeterImage(String meterImage) {
+		this.meterImage = meterImage;
+	}
+	public String getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+	public String getGrayImage() {
+		return grayImage;
+	}
+	public void setGrayImage(String grayImage) {
+		this.grayImage = grayImage;
+	}
+	
+	
+}
